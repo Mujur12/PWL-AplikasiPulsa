5f01199541dc1e9480a1ff74941f4d41062eca0b
Pieter Haro <pieter.haro@gmail.com>
2020-04-17 10:15:19 +0700

    crud barang

--- application/controllers/Barang.php
+++ application/controllers/Barang.php
@@ -35,4 +35,31 @@ class Barang extends CI_Controller {
 		$this->ModelBarang->insert($barang);
 		redirect("barang");
 	}
+
+	public function update($idBarang) {
+		$barang = $this->ModelBarang->getByPrimaryKey($idBarang);
+		$data = array(
+			"header" => "Barang",
+			"page" => "content/barang/v_update_barang",
+			"barang" => $barang
+		);
+		$this->load->view("layout/main", $data);
+	}
+	public function proses_update() {
+		$id = $this->input->post("id");
+		$barang = array(
+			"kode_barang" => $this->input->post("kode"),
+			"nama_barang" => $this->input->post("nama"),
+			"harga_barang" => $this->input->post("harga"),
+			"stock_barang" => $this->input->post("stock"),
+		);
+		$this->ModelBarang->update($id,$barang);
+		redirect("barang");
+	}
+
+	public function proses_hapus() {
+		$id = $this->input->post("id");
+		$this->ModelBarang->delete($id);
+		redirect("barang");
+	}
 }
--- application/views/content/barang/v_list_barang.php
+++ application/views/content/barang/v_list_barang.php
@@ -26,12 +26,17 @@
 					<td><?= $barang->nama_barang ?></td>
 					<td><?= $barang->harga_barang ?></td>
 					<td><?= $barang->stock_barang ?></td>
-					<td><img src="" width="150" alt=Gambar_Barang"/></td>
+					<td>
+						<img height="70"
+							 onerror="this.onerror=null;this.src='<?= base_url() . 'assets/images/no-image.png' ?>';"
+							 src="<?= $barang->gambar_barang ?>" alt="Gambar_Barang"/>
+					</td>
 					<td>
 						<a href="<?= site_url("barang/update/$barang->id_barang") ?>" class="btn btn-sm btn-warning">
 							<i class="fas fa-edit"></i>
 						</a>
-						<a href="#" class="btn btn-sm btn-danger btn-delete-barang"><i class="fas fa-trash"></i></a>
+						<a href="#" data-id="<?= $barang->id_barang ?>" class="btn btn-sm btn-danger btn-delete-barang"><i
+								class="fas fa-trash"></i></a>
 					</td>
 				</tr>
 				<?php
@@ -46,3 +51,41 @@
 		</a>
 	</div>
 </div>
+
+<div class="modal fade" id="modal-confirm-delete">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-body">
+				<h4>Anda Yakin Hapus data ini?</h4>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-info" data-dismiss="modal">Tidak</button>
+				<button type="button" class="btn btn-danger" id="btn-delete">Hapus</button>
+			</div>
+		</div>
+	</div>
+</div>
+<form id="form-delete" method="post" action="<?= site_url('barang/proses_hapus') ?>">
+
+</form>
+<script>
+    $(function () {
+        let idBarang = 0;
+        $(".btn-delete-barang").on("click", function () {
+            idBarang = $(this).data("id");
+            console.log(idBarang);
+            $("#modal-confirm-delete").modal("show");
+        });
+        $("#btn-delete").on("click", function () {
+//panggil url untuk hapus data
+            let inputId = $("<input>")
+                .attr("type", "hidden")
+                .attr("name", "id")
+                .val(idBarang);
+            let formDelete = $("#form-delete");
+            formDelete.empty().append(inputId);
+            formDelete.submit();
+            $("#modal-confirm-delete").modal("hide");
+        });
+    });
+</script>
